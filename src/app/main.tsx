cf5a65d6684a4d8bf4bb8e47961efcad4385a560
Smail000 <shibalenkov.kir@gmail.com>
2025-07-29 02:43:58 +0300

    setup tanstack query

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,6 +1,8 @@
 
+import { queryClient } from '@/app/config'
 import { router } from '@/app/router'
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
@@ -8,9 +10,11 @@ import { RouterProvider } from 'react-router'
 export function EntryPoint() {
     return (
         <StrictMode>
-            <ChakraProvider value={defaultSystem}>
-                <RouterProvider router={router} />
-            </ChakraProvider>
+            <QueryClientProvider client={queryClient}>
+                <ChakraProvider value={defaultSystem}>
+                    <RouterProvider router={router} />
+                </ChakraProvider>
+            </QueryClientProvider>
         </StrictMode>
     )
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,24 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Spinner } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
 
 
 export default function HomePage() {
+
+    const { isPending, isError, data, error } = useQuery({
+        queryKey: ["auth"],
+        queryFn: () => fetch("/api/v1/auth/me", { method: "GET" }).then(value => value.json())
+    })
+
     return (
         <Box w={"100%"} p={"10px"} display={"flex"} gap={"10px"}>
-            <Button>Click me</Button>
-            <Button>Click me</Button>
+            {
+                isPending ? <Spinner /> :
+                isError ? JSON.stringify(error) :
+                JSON.stringify(data)
+            }
+            <Button
+                onClick={async () => fetch("/api/v1/auth/logout", { method: "POST" })}
+            >Logout</Button>
         </Box>
     )
 }
